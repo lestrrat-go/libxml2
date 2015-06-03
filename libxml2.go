ceea5ae4582ebcd3f27764ac434236644d767fba
Daisuke Maki <lestrrat+github@gmail.com>
2015-06-03 18:41:28 +0900

    a bit more doc

--- libxml2.go
+++ libxml2.go
@@ -4,7 +4,18 @@ libxml2 package an interface to libxml2, providing XML and HTML parsers
 with DOM interface. The inspiration is Perl5's XML::LibXML module.
 
 This library is still in very early stages of development. API may still change
-without notice
+without notice.
+
+For the time being, the API is being written so that thye are as close as we
+can get to DOM Layer 3, but some methods will, for the time being, be punted
+and aliases for simpler methods that don't necessarily check for the DOM's
+correctness will be used.
+
+For example, `AppendChild()` must perform a lot of checks before returning
+successfully, but as of this writing it's just an alias for `xmlAddChild()`
+which does lots of... interesting things if you're not careful.
+
+Also, the return values are still shaky -- I'm still debating how to handle error cases gracefully.
 
 */
 package libxml2
