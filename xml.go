b61d7046b3b301ccd869cf2e8ef3ea59666a6d8b
Daisuke Maki <lestrrat+github@gmail.com>
2015-06-03 11:32:09 +0900

    Add Parse() methods that interface with xmlParseDoc

---
+++ xml.go
@@ -0,0 +1,32 @@
+package libxml2
+
+/*
+#cgo pkg-config: libxml-2.0
+#include "libxml/parser.h"
+*/
+import "C"
+import (
+	"bytes"
+	"errors"
+	"io"
+)
+
+func ParseString(s string) (*XmlDoc, error) {
+	doc, err := C.xmlParseDoc(stringToXmlChar(s))
+	if err != nil {
+		return nil, err
+	}
+	if doc == nil {
+		return nil, errors.New("parse failed")
+	}
+	return wrapXmlDoc(doc), nil
+}
+
+func Parse(in io.Reader) (*XmlDoc, error) {
+	buf := &bytes.Buffer{}
+	if _, err := buf.ReadFrom(in); err != nil {
+		return nil, err
+	}
+
+	return ParseString(buf.String())
+}
