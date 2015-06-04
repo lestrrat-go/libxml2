e11510ec1affb65d430d2055f0d1d0077ae7e034
Daisuke Maki <lestrrat+github@gmail.com>
2015-06-04 17:02:06 +0900

    Update examples

--- libxml2_example_test.go
+++ libxml2_example_test.go
@@ -7,6 +7,33 @@ import (
 	"github.com/lestrrat/go-libxml2"
 )
 
+func ExmapleXML() {
+	res, err := http.Get("http://blog.golang.org/feed.atom")
+	if err != nil {
+		panic("failed to get blog.golang.org: " + err.Error())
+	}
+
+	p := &libxml2.Parser{}
+	doc, err := p.Parse(res.Body)
+	defer res.Body.Close()
+
+	if err != nil {
+		panic("failed to parse XML: " + err.Error())
+	}
+	defer doc.Free()
+
+	doc.Walk(func(n libxml2.Node) error {
+		log.Printf(n.NodeName())
+		return nil
+	})
+
+	// XML with namespaces needs XPathContext, and we haven't
+	// gotten around to implementing it yet
+	//
+	// doc.FindNodes(...)
+}
+
+
 func ExampleHTML() {
 	res, err := http.Get("http://golang.org")
 	if err != nil {
