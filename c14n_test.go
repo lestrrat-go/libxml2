41069cc4f37d4d809c0752eeb21c190f644b6a59
Daisuke Maki <lestrrat+github@gmail.com>
2015-10-26 14:15:09 +0900

    Add some more testing

--- c14n_test.go
+++ c14n_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestC14N(t *testing.T) {
 	p := &Parser{}
 	doc, err := p.ParseString(`<?xml version="1.0"?>
-<Root>
+<Root xmlns="uri:go-libxml2:test" xmlns:test2="uri:go-libxml2:test2">
 	<EmptyElement foo="bar"/>
 </Root>`)
 
@@ -21,3 +21,24 @@ func TestC14N(t *testing.T) {
 	}
 	t.Logf("%s", s)
 }
+
+func TestC14NNonExclusive(t *testing.T) {
+	p := &Parser{}
+	doc, err := p.ParseString(`<?xml version="1.0"?>
+<Root xmlns="uri:go-libxml2:test" xmlns:test2="uri:go-libxml2:test2">
+	<EmptyElement foo="bar"/>
+</Root>`)
+
+	if err != nil {
+		t.Errorf("Failed to parse document: %s", err)
+		return
+	}
+
+	s, err := doc.ToStringC14N(false)
+	if err != nil {
+		t.Errorf("Failed to format in C14N: %s", err)
+		return
+	}
+	t.Logf("%s", s)
+}
+
