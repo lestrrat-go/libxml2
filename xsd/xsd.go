0826a8b3b6efa85cbd59234cac5ec1792a150b7f
Daisuke Maki <lestrrat+github@gmail.com>
2015-11-19 20:00:32 +0900

    Schema can now return the contents of the error

--- xsd/xsd.go
+++ xsd/xsd.go
@@ -3,6 +3,78 @@ package xsd
 /*
 #cgo pkg-config: libxml-2.0
 #include <libxml/xmlschemas.h>
+#include <string.h>
+#include <stdio.h>
+
+#define GO_LIBXML2_ERRWARN_ACCUMULATOR_SIZE 32
+typedef struct go_libxml2_errwarn_accumulator {
+	char *errors[GO_LIBXML2_ERRWARN_ACCUMULATOR_SIZE];
+	char *warnings[GO_LIBXML2_ERRWARN_ACCUMULATOR_SIZE];
+	int erridx;
+	int warnidx;
+} go_libxml2_errwarn_accumulator;
+
+static
+go_libxml2_errwarn_accumulator*
+MY_createErrWarnAccumulator() {
+	go_libxml2_errwarn_accumulator *ctx;
+	ctx = (go_libxml2_errwarn_accumulator *) malloc(sizeof(go_libxml2_errwarn_accumulator));
+	for (int i = 0; i < GO_LIBXML2_ERRWARN_ACCUMULATOR_SIZE; i++) {
+		ctx->errors[i] = NULL;
+		ctx->warnings[i] = NULL;
+	}
+	ctx->erridx = 0;
+	ctx->warnidx = 0;
+	return ctx;
+}
+
+static
+void
+MY_freeErrWarnAccumulator(go_libxml2_errwarn_accumulator* ctx) {
+	for (int i = 0; i < GO_LIBXML2_ERRWARN_ACCUMULATOR_SIZE; i++) {
+		if (ctx->errors[i] != NULL) {
+			free(ctx->errors[i]);
+		}
+		if (ctx->warnings[i] != NULL) {
+			free(ctx->errors[i]);
+		}
+	}
+	free(ctx);
+}
+
+static
+void
+MY_accumulateErr(void *ctx, const char *msg, ...) {
+  char buf[1024];
+  va_list args;
+	go_libxml2_errwarn_accumulator *accum;
+	int len;
+
+	accum = (go_libxml2_errwarn_accumulator *) ctx;
+	if (accum->erridx >= GO_LIBXML2_ERRWARN_ACCUMULATOR_SIZE) {
+		return;
+	}
+
+  va_start(args, msg);
+  len = vsnprintf(buf, sizeof(buf), msg, args);
+  va_end(args);
+
+	if (len == 0) {
+		return;
+	}
+
+	char *out = (char *) malloc(sizeof(char) * len);
+	memcpy(out, buf, len);
+  int i = accum->erridx++;
+	accum->errors[i] = out;
+}
+
+static
+void
+MY_setErrWarnAccumulator(xmlSchemaValidCtxtPtr ctxt, go_libxml2_errwarn_accumulator *accum) {
+	xmlSchemaSetValidErrors(ctxt, MY_accumulateErr, NULL, accum);
+}
+
 */
 import "C"
 import (
@@ -34,10 +106,22 @@ func Parse(buf []byte) (*Schema, error) {
 	return &Schema{ptr: s}, nil
 }
 
-func (s *Schema) Close() {
+func (s *Schema) Free() {
 	if ptr := s.ptr; ptr != nil {
 		C.xmlSchemaFree(ptr)
 	}
+}
+
+type SchemaValidationError struct {
+	errors []error
+}
+
+func (sve SchemaValidationError) Error() string {
+	return "schema validation failed"
+}
+
+func (sve SchemaValidationError) Errors() []error {
+	return sve.errors
 }
 
 func (s *Schema) Validate(d *libxml2.Document) error {
@@ -47,8 +131,20 @@ func (s *Schema) Validate(d *libxml2.Document) error {
 	}
 	defer C.xmlSchemaFreeValidCtxt(ctx)
 
+	accum := C.MY_createErrWarnAccumulator()
+	defer C.MY_freeErrWarnAccumulator(accum)
+
+	C.MY_setErrWarnAccumulator(ctx, accum)
+
 	if C.xmlSchemaValidateDoc(ctx, (C.xmlDocPtr)(d.Pointer())) != 0 {
-		return errors.New("failed to validate document")
+		// Create an err
+		err := SchemaValidationError{
+			errors: make([]error, 0, accum.erridx),
+		}
+		for i := 0; i < int(accum.erridx); i++ {
+			err.errors = append(err.errors, errors.New(C.GoString(accum.errors[i])))
+		}
+		return err
 	}
 	return nil
 }
---
+++ xsd_test.go
@@ -0,0 +1,116 @@
+package libxml2_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/lestrrat/go-libxml2"
+	"github.com/lestrrat/go-libxml2/xsd"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestXSD(t *testing.T) {
+	xsdfile := filepath.Join("test", "xmldsig-core-schema.xsd")
+	f, err := os.Open(xsdfile)
+	if !assert.NoError(t, err, "open schema") {
+		return
+	}
+	defer f.Close()
+
+	buf, err := ioutil.ReadAll(f)
+	if !assert.NoError(t, err, "reading from schema") {
+		return
+	}
+
+	s, err := xsd.Parse(buf)
+	if !assert.NoError(t, err, "parsing schema") {
+		return
+	}
+	defer s.Free()
+
+	func() {
+		d, err := libxml2.ParseString(`<foo></foo>`)
+		if !assert.NoError(t, err, "parsing XML") {
+			return
+		}
+		defer d.Free()
+
+		err = s.Validate(d)
+		if !assert.Error(t, err, "s.Validate should fail") {
+			return
+		}
+
+		serr, ok := err.(xsd.SchemaValidationError)
+		if !assert.True(t, ok, "error is xsd.SchemaValidationErr") {
+			return
+		}
+
+		if !assert.Len(t, serr.Errors(), 1, "there's one error") {
+			return
+		}
+		for _, e := range serr.Errors() {
+			t.Logf("err (OK): %s", e)
+		}
+	}()
+
+	func() {
+		const src = `<?xml version="1.0" encoding="UTF-8"?>
+  <Signature xmlns="http://www.w3.org/2000/09/xmldsig#">
+    <SignedInfo>
+      <CanonicalizationMethod 
+           Algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-
+20010315#WithComments"/>
+      <SignatureMethod Algorithm="http://www.w3.org/2000/09/
+xmldsig#dsa-sha1"/>
+      <Reference URI="">
+        <Transforms>
+          <Transform Algorithm="http://www.w3.org/2000/09/
+xmldsig#enveloped-signature"/>
+        </Transforms>
+        <DigestMethod Algorithm="http://www.w3.org/2000/09/
+xmldsig#sha1"/>
+        <DigestValue>uooqbWYa5VCqcJCbuymBKqm17vY=</DigestValue>
+      </Reference>
+    </SignedInfo>
+<SignatureValue>
+KedJuTob5gtvYx9qM3k3gm7kbLBwVbEQRl26S2tmXjqNND7MRGtoew==
+    </SignatureValue>
+    <KeyInfo>
+      <KeyValue>
+        <DSAKeyValue>
+          <P>
+/KaCzo4Syrom78z3EQ5SbbB4sF7ey80etKII864WF64B81uRpH5t9jQTxe
+Eu0ImbzRMqzVDZkVG9xD7nN1kuFw==
+          </P>
+          <Q>li7dzDacuo67Jg7mtqEm2TRuOMU=</Q>
+          <G>Z4Rxsnqc9E7pGknFFH2xqaryRPBaQ01khpMdLRQnG541Awtx/
+XPaF5Bpsy4pNWMOHCBiNU0NogpsQW5QvnlMpA==
+          </G>
+          <Y>qV38IqrWJG0V/
+mZQvRVi1OHw9Zj84nDC4jO8P0axi1gb6d+475yhMjSc/
+BrIVC58W3ydbkK+Ri4OKbaRZlYeRA==
+         </Y>
+        </DSAKeyValue>
+      </KeyValue>
+    </KeyInfo>
+  </Signature>
+`
+		d, err := libxml2.ParseString(src)
+		if !assert.NoError(t, err, "parsing XML") {
+			return
+		}
+		defer d.Free()
+
+		err = s.Validate(d)
+		if !assert.NoError(t, err, "s.Validate should pass") {
+			if serr, ok := err.(xsd.SchemaValidationError); ok {
+				for _, e := range serr.Errors() {
+					t.Logf("err: %s", e)
+				}
+			}
+			return
+		}
+	}()
+}
